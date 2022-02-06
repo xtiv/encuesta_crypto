cc88fb1cc352c2d2887c60018e9baff894169f4b
xtiv <xtiv@mail.com>
2022-02-06 21:17:00 +0100

    css styles pt1

--- awardsRanking/encuestas/admin.py
+++ awardsRanking/encuestas/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-from encuestas.models import Pregunta
-admin.site.register(Pregunta)
+from encuestas.models import Pregunta, Opciones
+
+class OpcionesInline(admin.StackedInline):
+    model = Opciones
+    extra = 2
+    
+class PreguntaAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "pregunta_text"]
+    inlines = [OpcionesInline]
+    list_display = ("pregunta_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["pregunta_text"]
+
+admin.site.register(Pregunta, PreguntaAdmin)
--- awardsRanking/encuestas/models.py
+++ awardsRanking/encuestas/models.py
@@ -22,3 +22,4 @@ class Opciones(models.Model):
     def __str__(self):
         return self.opcion_text
 
+
